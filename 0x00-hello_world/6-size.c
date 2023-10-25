432d277ae3b082a3755a42f871edce1602296081
primitive12 <oueslatiahmed22@gmail.com>
2023-10-25 07:11:42 -0700

    combination of printf&sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints the size of vars.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+printf("Size of a char:%d byte(s)\n", sizeof(char));
+ptintf("Size of an int:%d byte(s)\n", sizeof(int));
+printf("Size of a long int:%d byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%d byte(s)\n", sizeof(long long int));
+printf("Size of a float:%d byte(s)\n", sizeof(float));
+return (0);
+}
