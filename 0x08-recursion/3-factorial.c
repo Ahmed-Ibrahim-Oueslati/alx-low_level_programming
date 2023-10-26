b32f5bef081e741a20e8a71b461b06c1f58c70eb
primitive12 <oueslatiahmed22@gmail.com>
2023-10-25 17:31:31 -0700

    factorial of a nb

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to calculate the factorial of
+ * Return: the factorial of n, -1 if n is < 0, 1 if n = 0
+ */
+int factorial(int n)
+{
+	int fac;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	fac = n * factorial(n - 1);
+	return (fac);
+}
