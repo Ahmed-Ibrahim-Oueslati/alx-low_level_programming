0af77be898c8698cc1c60bc23a4b2c82fed8a206
primitive12 <oueslatiahmed22@gmail.com>
2023-10-27 07:43:36 -0700

    uppercase with ascci code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i=97,k=65 ;
+
+        while (i<123)
+        {
+        putchar(i);
+        i++;
+        }
+	  while (k<90)
+        {
+        putchar(k);
+        k++;
+        }
+        putchar(10);
+return (0);
+}
