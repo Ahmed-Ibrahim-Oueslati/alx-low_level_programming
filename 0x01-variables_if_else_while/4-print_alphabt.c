56399b0488b8996d8a86c249bb47d4da149d76d1
primitive12 <oueslatiahmed22@gmail.com>
2023-10-27 07:52:25 -0700

    first nested if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i=97 ;              
+        while (i<123)
+	{
+	if (i!=113 && i!=101)
+	{
+	putchar(i);
+	
+	}
+	i++;
+	}
+	putchar(10);	
+return (0);   
+}
