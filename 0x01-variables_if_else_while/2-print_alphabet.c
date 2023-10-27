21c0a4119983ee6fde1311a4ec5328d3101cd252
primitive12 <oueslatiahmed22@gmail.com>
2023-10-27 07:38:29 -0700

    putchar&ascii code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i=97 ;              
+        while (i<123)
+	{
+	putchar(i);
+	i++;
+	}
+	putchr("\n");	
+return (0);   
+}
