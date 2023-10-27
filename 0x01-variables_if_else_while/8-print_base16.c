5a1583391a20538950474d503c1762cac2254a70
primitive12 <oueslatiahmed22@gmail.com>
2023-10-27 08:01:01 -0700

    reusing my own code 3

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i=48,k=97 ;
+
+        while (i<58)
+        {
+        putchar(i);
+        i++;
+        }
+	  while (k<103)
+        {
+        putchar(k);
+        k++;
+        }
+        putchar(10);
+return (0);
+}
