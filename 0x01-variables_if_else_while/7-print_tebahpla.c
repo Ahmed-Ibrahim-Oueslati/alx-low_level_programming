2955a4d4d2b4a2857fc1a2fb2caed5bf11025e21
primitive12 <oueslatiahmed22@gmail.com>
2023-10-27 07:58:34 -0700

    reverrse the count

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i=122 ;              
+        while (i>96)
+	{
+	putchar(i);
+	i--;
+	}
+	putchar(10);	
+return (0);   
+}
