312796013213d4667f88bcdfda6eebb53b4a31ba
primitive12 <oueslatiahmed22@gmail.com>
2023-10-28 08:25:36 -0700

    test101e

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,38 @@
  */
 int main(void)
 {
-int i=48, k=48,j=48 ;              
-        while (i<56 )
+int i=48, k=48,j=48, p=48;              
+        while (i<55 )
 	{
-	while(k<57)
+	while(k<56)
 	{
 	 if (k!=i && k>i)
 	 {
-	while (j<58)
+	while (j<57)
 	{
 	if (j!=k && j>k)
 	{
+	while (p<58)
+	{
+	if (p!=j && p >j)
+	
+	{
 	putchar(i);
 	putchar(k);
+	putchar(32);
 	putchar(j);
+	putchar(p);
 	if (i!=55)
 	{
 	putchar(44);
 	putchar(32);
 	}
 	}
+	p++;
+	}
+	}
 	j++;
+	p =p-10;
 	}
 	 }
 	k++;
