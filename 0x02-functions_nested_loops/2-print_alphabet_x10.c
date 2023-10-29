528ab524e1f0af43a6abad73b8984a0a30b458b6
primitive12 <oueslatiahmed22@gmail.com>
2023-10-28 18:17:51 -0700

    galegg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,15 @@
 #include "./main.h"
+void print_alphabet(void)
+{
+
+int i=97 ;
+        while (i<123)
+        {
+       _putchar(i);
+        i++;
+        }
+        _putchar(10);
+}
 void print_alphabet_x10(void)
 {
 	int i = 0;
