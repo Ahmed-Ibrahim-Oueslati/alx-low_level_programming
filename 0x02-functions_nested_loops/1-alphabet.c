5579f95f0c73480a343cecb2e377136799804e7a
primitive12 <oueslatiahmed22@gmail.com>
2023-10-28 17:03:30 -0700

    test747

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
-
+#include <main.h>
 void print_alphabet(void)
 {
 
 int i=97 ;
         while (i<123)
         {
-        _putchar(i);
+        putchar(i);
         i++;
         }
-        _putchar(10);
+        putchar(10);
 return (0);
 }
