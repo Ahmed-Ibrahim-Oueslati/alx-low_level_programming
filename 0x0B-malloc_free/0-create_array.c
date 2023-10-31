3d7924baaa55e03a5dc3c96939445efa60e460c5
primitive12 <oueslatiahmed22@gmail.com>
2023-10-31 15:41:33 -0700

    fixing space for betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,17 @@
  *       */
 char *create_array(unsigned int size, char c)
 {
-		char *p;
-			unsigned int i;
+char *p;
+unsigned int i;
 
-				p = malloc(size * sizeof(char));
-					if (size == 0 || p == NULL)
-							{
-										return (NULL);
-											}
-						for (i = 0; i < size; i++)
-								{
-										p[i] = c;
-											}
-							return (p);
+p = malloc(size * sizeof(char));
+if (size == 0 || p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return (p);
 }
