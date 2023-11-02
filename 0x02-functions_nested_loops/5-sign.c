08266ebd145f14a64e6514764264865d8ae16805
primitive12 <oueslatiahmed22@gmail.com>
2023-11-02 06:38:23 -0700

    checking

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int print_sign(int n)
+{
+             if (n>0)
+	     {
+		     return(1);
+	     }
+	     if (n=0)
+	     {
+		     return(0);
+	     }
+	     if (n<0)
+	     {
+		     return(-1);
+	     }
+             
+return (0);   
+}
