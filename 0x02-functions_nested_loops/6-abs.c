de79deb72d55f6bf9bfed267ff1c5cd398fea7f1
primitive12 <oueslatiahmed22@gmail.com>
2023-11-02 07:11:39 -0700

    absolute power

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "./main.h"
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int _abs(int)
+{
+        if (n>0)
+        {
+                return(n);
+        }
+
+        if (n ==0)
+        {
+                return(0);
+        }
+          
+        if ( n<0)
+        {
+		return(-n);
+	}
+           
+
+return (0);
+}
+~ 
