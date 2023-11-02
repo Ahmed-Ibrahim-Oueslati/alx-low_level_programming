e9ab39a2bc89d4002630f22ec263245b77e6444c
primitive12 <oueslatiahmed22@gmail.com>
2023-11-02 07:22:08 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "./main.h"
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int print_last_digit(int n);
+
+{
+int 	p=n%10;
+if (n==0)
+{
+	return(0);
+}
+
+if ((n%10)!=0)
+{
+	return(p);
+}
+             
+return (0);   
+}
