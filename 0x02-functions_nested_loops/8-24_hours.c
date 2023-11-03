762db0ab293f51b72b7ff6c7a33193ac525216f2
primitive12 <oueslatiahmed22@gmail.com>
2023-11-03 16:26:09 -0700

    chezking789

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+void jack_bauer(void)
 {
 int i=48, k=48 , j=48 ,p=48;
-        while (i<58)
+        while (i<=50)
         {
 
-        putchar(i);
-        putchar(k);
-        putchar(32);
-        putchar(j);
-        putchar(p);
+        _putchar(i);
+        _putchar((k);
+        _putchar(32);
+        _putchar(j);
+        _putchar(p);
 
         putchar(44);
         putchar(32);
@@ -19,9 +30,9 @@ int i=48, k=48 , j=48 ,p=48;
 
         j++;
         }
-        if (j==58)
+        if (j==54)
         {
-        j =j-10;
+        j =j-5;
         k++;
         }
         if (k==58)
@@ -33,13 +44,4 @@ int i=48, k=48 , j=48 ,p=48;
         putchar(10);
 return (0);
 }
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
+             
