8f345b34e49c18ab5b3f29ed00d1504d56d3fa03
primitive12 <oueslatiahmed22@gmail.com>
2023-11-03 15:45:52 -0700

    print the last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -7,7 +7,12 @@
  */
 int main(void)
 {
-     print_last_digit(98);
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
     _putchar('\n');
     return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,34 @@
  *
  * Return: Always 0 (success).
  */
+int _abs(int n)
+{
+
+        if (n>0)
+        {
+                return(n);
+        }
+
+        if (n ==0)
+        {
+                return(0);
+        }
+
+        if ( n<0)
+        {
+                return(-n);
+        }
+
+
+return (0);
+}
 int print_last_digit(int n)
 
 {
+	int j = _abs(n);
 
-int p = n%10;
+
+int p = j%10;
 	
 	_putchar(p +'0');
 return(0);
