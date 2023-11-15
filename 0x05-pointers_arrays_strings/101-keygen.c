3c226c3bf89b9079ba9de67adfc5eaaa1a2882aa
primitive12 <oueslatiahmed22@gmail.com>
2023-11-15 02:14:12 -0800

    update the file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,16 @@
-hhhhhh
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+     char  a[5]="hhh";     
+             
+return (a);   
+}
