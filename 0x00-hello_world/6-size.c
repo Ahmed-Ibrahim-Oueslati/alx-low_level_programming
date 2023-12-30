a73e32b57ca634956dd59da9f9f689884acc746a
primitive12 <oueslatiahmed22@gmail.com>
2023-12-30 14:51:20 -0800

    first try of size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	int i; 
+	float f; 
+	char v ; 
+	long int li ; 
+	long long int lli;
+printf("Size of a char: %c byte(s)",v);
+printf("Size of an int: %d  byte(s)",i);
+printf("Size of a long int: %d  byte(s)",li);
+printf("Size of a long long int: %d  byte(s)",lli);
+printf("Size of a float: %z byte(s)"),f;
+return (0);
+}
