615a45abe9a6980a66347218bbd2a04c49b2c72e
primitive12 <oueslatiahmed22@gmail.com>
2023-12-30 14:42:05 -0800

    first use of printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
