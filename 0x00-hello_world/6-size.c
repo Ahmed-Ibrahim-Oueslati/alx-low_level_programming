b8be3089705ea5319340b6a7906d19fb78890166
primitive12 <oueslatiahmed22@gmail.com>
2023-12-30 14:57:28 -0800

    first try of sizeof 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int i; 
 	float f; 
-	char v ; 
+	char v; 
 	long int li ; 
 	long long int lli;
 printf("Size of a char: %c byte(s)\n",v);
 printf("Size of an int: %d  byte(s)\n",i);
 printf("Size of a long int: %ld  byte(s)\n",li);
 printf("Size of a long long int: %lld  byte(s)\n",lli);
-printf("Size of a float: %f byte(s)\n"),f;
+printf("Size of a float: %f byte(s)\n",f);
 return (0);
 }
