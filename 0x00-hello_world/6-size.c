c3386eccae7b134bc2212709f3c48ec753f76740
primitive12 <oueslatiahmed22@gmail.com>
2023-12-30 15:02:49 -0800

    first try of sizeof 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	char v; 
 	long int li ; 
 	long long int lli;
-printf("Size of a char: %c byte(s)\n",v);
+printf("Size of a char: %lu byte(s)\n",sizeof(v));
 printf("Size of an int: %d  byte(s)\n",i);
 printf("Size of a long int: %ld  byte(s)\n",li);
 printf("Size of a long long int: %lld  byte(s)\n",lli);
