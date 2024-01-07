e6d4f264d3e989ab8576bd7bdb799ec80749e7aa
primitive12 <oueslatiahmed22@gmail.com>
2024-01-07 09:52:52 -0800

    print the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,21 @@
 /**
  * main - Entry point of the program.
  *
- * Description: Prints a string to the console.
+ * Description: Prints the alphabet to the consol.
  *
  * Return: Always 0 (success).
  */
 int main(void)
 {
-             
-             
-return (0);   
+int n=97;
+while (n < 123)
+{
+putchar(n);
+if (n == 122)
+{
+putchar('\n');
+}
+n = n+1;
+}
+return (0);
 }
