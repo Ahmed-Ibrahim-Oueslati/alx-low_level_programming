eea58629aaf3d6d1fdf6be540733c7a821f49361
primitive12 <oueslatiahmed22@gmail.com>
2024-01-07 10:51:58 -0800

    prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints the alphabets in reverse.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int n = 122;
+while (n > 98)
+{
+putchar(n);
+if (n == 97)
+{
+putchar('\n');
+}
+n = n - 1;
+}
+return (0);
+}
