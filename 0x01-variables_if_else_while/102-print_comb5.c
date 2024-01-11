db007b624470c6f39464df895f9b4e91b1f9043f
primitive12 <oueslatiahmed22@gmail.com>
2024-01-11 03:05:56 -0800

    printing the combination of 4 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,31 +9,28 @@
  */
 int main(void)
 {
-	
 
 
-    int n, j, u, y;
-    for (n = 48; n < 58; n++) {
-        for (j = 48; j < 58; j++) {
-            for (u = 48; u < 58; u++) {
-                for (y = 48; y < 58; y++) {
-                    if ((n < u) || ((n == u) && (j < y))) {
-                        putchar(n);
-                        putchar(j);
-                        putchar(u);
-                        putchar(y);
-                        if (n != 57 || j != 56 || u != 57 || y != 57) {
-                            putchar(44);
-                            putchar(32);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-
-
-
-return (0);   
+int n, j, u, y;
+for (n = 48; n < 58; n++) {
+for (j = 48; j < 58; j++) {
+for (u = 48; u < 58; u++) {
+for (y = 48; y < 58; y++) {
+if ((n < u) || ((n == u) && (j < y))) {
+putchar(n);
+putchar(j);
+putchar(32);
+putchar(u);
+putchar(y);
+if (n != 57 || j != 56 || u != 57 || y != 57) {
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
