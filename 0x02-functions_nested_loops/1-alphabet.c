d7caf06e402fb22da4464077f65be8725527caa4
primitive12 <oueslatiahmed22@gmail.com>
2024-01-12 05:29:41 -0800

    printing the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints the alphabet in lowercase to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int n = 97;
+while (n < 122)
+{
+putchar(n);
+if (n == 122)
+putchar('\n');
+n++;
+}
+return (0);
+}
