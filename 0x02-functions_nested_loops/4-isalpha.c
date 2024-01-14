b99f5ce5aa3f1e65cea711619ef47c4e4c7cb512
primitive12 <oueslatiahmed22@gmail.com>
2024-01-14 10:00:25 -0800

    checks an int is an alphabetic character  or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - Entry point of the program.
+ *
+ * Description: checks if an int  c is an alphabetic character
+ * Return: 1 (success).
+* @c: c is the int  to check (parameter)
+ */
+int _isalpha(int c)
+{
+if ((c > 64 && c< 91) || (c > 96 && c < 123))
+{
+return (1);
+}
+else
+return (0);
+}
