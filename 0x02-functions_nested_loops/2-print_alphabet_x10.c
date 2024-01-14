c61f7df5c87987bb81a10edab98b5372248a39e2
primitive12 <oueslatiahmed22@gmail.com>
2024-01-14 08:46:16 -0800

    printing the alphabet ten times lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Entry point of the program.
+ *
+ * Description: Prints the alphabet in lowercase 10 times  to the console.
+ *
+ * Return: Always 0 (success).
+ */
+void print_alphabet_x10(void)
+{
+n = 1
+while (n < 11)
+{
+print_alphabet();
+if (n < 10)
+{
+putchar('\n');
+}
+n++;
+}
+}
