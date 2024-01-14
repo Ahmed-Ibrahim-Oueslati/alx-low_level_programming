0cb8bcf5af3090cccd3ffade1c597879372a7318
primitive12 <oueslatiahmed22@gmail.com>
2024-01-14 09:47:58 -0800

    checks the character is lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,12 @@
 /**
  * _islower - Entry point of the program.
  *
- * Description: checks if a character is lowercase or not 
+ * Description: checks if a character c is lowercase or not 
  *
  * Return: Always 0 (success).
  */
 int _islower(int c)
 {
-int c ;
 if (c > 96 && c < 123)
 {
 return (1);
