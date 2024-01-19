7cdb02f7293dc3d5fab2bde80e59af7ae3f4ff0a
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 15:54:56 -0800

    dumb way to print the minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,6 +21,12 @@ for (l = 0; l < 10; l++)
 {
 if (i == 2 && j == 3 && k == 5 && l == 9)
 {
+_putchar('0' + i);
+_putchar('0' + j);
+_putchar(58);
+_putchar('0' + k);
+_putchar('0' + l);
+_putchar(10);
 /*Stop the loop when all four variables reach specified values*/
 return;
 }
