d7a2949f12acb311f671143dc4f53a95a942baba
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 14:48:56 -0800

    prints the last digit of a nb

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - prints the last difit of a nb
+ * Return: a postive int
+ * @n: n is the number to check (parameter)
+ */
+int print_last_digit(int n)
+{
+int k ; 
+if (n < 0)
+{
+n = n * -1;
+}
+k = n % 10;
+return (k);
+}
