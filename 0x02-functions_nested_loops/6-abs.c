f0eb0f26edee3bbf7083cd25c13584d7ffa02ad7
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 14:42:53 -0800

    the absolute vlue of x or |x|

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _abs - prints the absolute value of an integer 
- * Return: a positve int 
+ * _abs - prints the absolute value of an integer
+ * Return: a positve int
  * @n: n is the number to check (parameter)
  */
 int _abs(int n)
@@ -14,7 +14,7 @@ return (n);
 }
 else if (n < 0)
 {
-n = n * -1 ;
+n = n * -1;
 return (n);
 }
 else
