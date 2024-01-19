7bc548cccbcda410b580b81a2e3d829b87efaf7c
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 11:55:21 -0800

    checks the polarity (+/-)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,11 @@
  *Returns 1 and prints + if n is greater than zero
  *Returns 0 and prints 0 if n is zero
  *Returns -1 and prints - if n is less than zero
- * 
+ * @n: n is the number to check (parameter) 
  */
 int print_sign(int n)
 {
 if (n > 0)
-
 {
 _putchar('+');
 return (1);
