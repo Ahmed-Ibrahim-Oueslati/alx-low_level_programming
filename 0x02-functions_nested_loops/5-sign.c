5208e40bf84c650d1958f4c0d9aabe1e68beab05
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 12:40:58 -0800

    checks the polarity (+/-) of the nb

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,9 @@
 #include <stdio.h>
 /**
  * print_sign - Entry point of the program.
- *
  * Description: prints the sign of a number.
  *Returns 1 and prints + if n is greater than zero
- *Returns 0 and prints 0 if n is zero
- *Returns -1 and prints - if n is less than zero
- * @n: n is the number to check (parameter) 
+ * @n: n is the number to check (parameter)
  */
 int print_sign(int n)
 {
