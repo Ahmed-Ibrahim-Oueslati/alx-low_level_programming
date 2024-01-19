f939d25824d5b3681e3fa019727689d0a4447bea
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 15:20:19 -0800

    second test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ int i,j,k,l;
 for (i = 0; i < 3; i++){
 for (j = 0; j < 4; j++){
 for (k = 0; k < 6; k++){
-for (l = 0; l < 10 l++){
+for (l = 0; l < 10; l++){
 _putchar(i);
 _putchar(j);
 _putchar(58);
