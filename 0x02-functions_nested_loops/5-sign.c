aee57f0797866b49047452df40df8c4aa95d8f40
primitive12 <oueslatiahmed22@gmail.com>
2024-01-19 11:52:01 -0800

    let's see3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,15 +18,14 @@ if (n > 0)
 putchar('+');
 return (1);
 }
-else if (n == 0)
+else if (n < 0)
 {
-putchar('0');
-return 0;
+putchar('-');
+return (-1);
 }
 else
 {
-putchar ('-');
-return -1;
-}
+putchar ('0');
 return (0);
+}
 }
