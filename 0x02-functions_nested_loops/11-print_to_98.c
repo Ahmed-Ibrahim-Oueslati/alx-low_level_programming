b838b1f1425927dc459b8e5e9ed53b0e839d625c
primitive12 <oueslatiahmed22@gmail.com>
2024-01-24 12:46:06 -0800

    chat do it

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,50 +1,33 @@
-#include <stdlib.h>
+nclude <stdlib.h>
 #include "main.h"
 #include <stdio.h>
+
 /**
- *  print_to_98 - Entry point of the program.
+ * print_to_98 - Entry point of the program.
  *
- * Description: printing nbs from n to 98 
+ * Description: printing nbs from n to 98
  *
- * Return: void 
- * @n parametre to configure 
+ * Return: void
+ * @n: parameter to configure
  */
 void print_to_98(int n)
 {
-if (n <= 98)
-
-{
-while (n < 99)
-{
-if (n != 98)
-{
-printf("%d, ", n);
-}
-else
-{ 
-printf("%d", n);
-}
-n++;
-}
-
-}
-
-
-if (n >= 98)
-
-{
-while (n > 97)
-{
-if (n != 98)
-{
-printf("%d, ", n);
-}
-else 
-{
-printf("%d ", n);
-}
-n--;
-}
+    if (n <= 98)
+    {
+        while (n < 98)
+        {
+            printf("%d, ", n);
+            n++;
+        }
+    }
+    else
+    {
+        while (n > 98)
+        {
+            printf("%d, ", n);
+            n--;
+        }
+    }
+    printf("%d\n", n); // Print the last number without a comma and space
 }
 
-}
