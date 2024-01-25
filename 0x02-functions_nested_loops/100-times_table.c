ddc8afca47e42f920e213ecedcb89f5ac57e31e2
primitive12 <oueslatiahmed22@gmail.com>
2024-01-25 04:32:13 -0800

    printing n times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point of the program.
+ * print_times_table - Entry point of the program.
  *
- * Description: Prints a string to the console.
+ * Description: Prints n times table
  *
- * Return: Always 0 (success).
+ * Return: void
+ *@n: parameter of the table
  */
 void print_times_table(int n)
 {
@@ -19,33 +20,28 @@ for (a = 0 ; a <= n ; a++)
 _putchar(48);
 for (b = 1 ; b <= n ; b++)
 {
-int u ; 
-u = b * a ;
+int u;
+u = b * a;
 _putchar(44);
 _putchar(32);
-if (u <= 9 )   
+if (u <= 9 )
 {
 _putchar(32);
 _putchar(32);
 _putchar(u + 48);
-} 
-
-else if ( u >= 10 &&  u <= 99)
+}
+else if (u >= 10 &&  u <= 99)
 {
 _putchar(32);
 _putchar((u / 10) + 48);
 _putchar((u % 10) + 48);
 }
-if ( u > 99) 
+if ( u > 99)
 {
-int i;
-int p;
-int k;
-int t;
-i = u / 100;
-p = u / 10;
-k = p % 10;
-t = u % 10;
+int i = u / 100;
+int p = u / 10;
+int k = p % 10;
+int t = u % 10;
 
 _putchar(i + 48);
 _putchar(k + 48);
