3ec3229e1d2aed7ff69db631c507f5ca22503354
primitive12 <oueslatiahmed22@gmail.com>
2024-01-25 06:17:34 -0800

    sum of M5 and M3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+ /**
+  * main  - Entry point of the program.
+  * Description:  prints the sum of all the multiples of 3 or 5 below 1024
+  * Return: 0 success
+ */
 int main(void)
 {
 int a;
@@ -12,6 +16,6 @@ if ((a % 5 == 0) || (a % 3 == 0))
 y = y + a;
 }
 }
-printf("%d \n", y);
+printf("%d\n", y);
 return (0);
 }
