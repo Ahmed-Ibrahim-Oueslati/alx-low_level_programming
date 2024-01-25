ecf7575ee7bfc77fc5174db827ac640799b4ff69
primitive12 <oueslatiahmed22@gmail.com>
2024-01-25 07:07:23 -0800

    print fibonaccia

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,5 +10,20 @@
  */
 int main(void)
 {
+int y;
+int u = 1;
+int p = 2;
+int k;
+printf("%d, ", u);
+printf("%d, ", p);
+for (y = 1 ; y < 50 ; y++)
+{
+k = p + u;
+printf("%d, ", k);
+
+u = p;
+p = k;
+}
+printf("\n");
 return (0);
 }
