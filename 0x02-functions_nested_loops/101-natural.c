6f7278e9a3bbcb336bec611704a5baa9bb9b9b37
primitive12 <oueslatiahmed22@gmail.com>
2024-01-25 06:13:51 -0800

    cheking hhhe

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int limit = 1024;
-
-    int n3 = (limit - 1) / 3;
-    int sum3 = 3 * n3 * (n3 + 1) / 2;
-
-    int n5 = (limit - 1) / 5;
-    int sum5 = 5 * n5 * (n5 + 1) / 2;
-
-    int n15 = (limit - 1) / 15;
-    int sum15 = 15 * n15 * (n15 + 1) / 2;
-
-    int finalSum = sum3 + sum5 - sum15;
-
-    printf("Sum of multiples of 3 and 5 up to %d: %d\n", limit, finalSum);
-
-    return 0;
+int main(void)
+{
+int a;
+int y = 0;
+for (a = 0; a < 1024; a++)
+{
+if ((a % 5 == 0) || (a % 3 == 0))
+{
+y = y + a;
 }
-
+}
+printf("%d \n", y);
+return (0);
+}
