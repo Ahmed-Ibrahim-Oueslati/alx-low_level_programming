454dc3d3a4197ba3b2eab5b39430d663356bb53e
primitive12 <oueslatiahmed22@gmail.com>
2024-01-26 02:49:13 -0800

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int y;
+long int u = 1;
+long int p = 2;
+long int k;
+printf("%ld, ", u);
+printf("%ld, ", p);
+for (y = 1 ; y < 97 ; y++)
+{
+k = p + u;
+if (y != 96)
+{
+printf("%ld, ", k);
+}
+else
+{
+printf("%ld", k);
+}
+u = p;
+p = k;
+}
+printf("\n");
+return (0);
+}
