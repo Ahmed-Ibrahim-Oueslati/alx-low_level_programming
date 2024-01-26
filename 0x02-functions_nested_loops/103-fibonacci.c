cc1453082b9eb865de633d074499337eef7ad039
primitive12 <oueslatiahmed22@gmail.com>
2024-01-26 01:22:00 -0800

    sum of even terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints the sum of even valued fibonacci terms (<400000)
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int y;
+long int u = 1;
+long int p = 2;
+long int k;
+long int m = 0;
+for (y = 1 ; y < 49 ; y++)
+{
+k = p + u;
+u = p;
+p = k;
+if ((k % 2 == 0) && (k < 4000000))
+{
+m = m + k ;
+}
+}
+printf("%ld\n", m);
+return (0);
+}
