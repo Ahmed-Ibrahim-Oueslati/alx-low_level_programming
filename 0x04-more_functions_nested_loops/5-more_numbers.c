329aca9a0db8feb4a3810a5ce444c19c749a707b
primitive12 <oueslatiahmed22@gmail.com>
2024-01-27 18:07:31 -0800

    printing 10 times from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - Entry point of the program.
+ *
+ * Description: Prints from 0 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+for (b = 0; b < 11; b++)
+{
+for (a = 0; a < 15; a++)
+{
+if (a < 10)
+{
+_putchar(a + '0');
+}
+else
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
