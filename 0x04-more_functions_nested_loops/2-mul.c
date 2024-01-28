e2544aa178d561a0ebdeeca81b50b7448282ff1e
primitive12 <oueslatiahmed22@gmail.com>
2024-01-27 17:46:40 -0800

    multiply 2 int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: int to multiply
+ *@b: int ot multiply
+ * Return:an int
+ */
+int mul(int a, int b)
+{
+int k;
+k = a * b;
+return (b);
+}
