0780b42701ae238f91990e2170cd6592f867ac00
primitive12 <oueslatiahmed22@gmail.com>
2024-01-28 05:21:57 -0800

    first test

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - Entry point of the program.
+ *
+ * Description: Prints a digonal line in the terminal
+ *@n: parameter to print
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+int p;
+int b;
+if (n > 0)
+{
+for (b = 0; b < n; b++)
+{
+for (p = 0; p < b; p++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar(10);
+}
+_putchar(10);
+}
+}
