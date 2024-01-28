8b9929af4f2668785311ddd7e2f42be1d55db1d3
primitive12 <oueslatiahmed22@gmail.com>
2024-01-28 04:24:12 -0800

    testuu

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,16 @@ int a;
 int b;
 for (b = 0; b < 10; b++)
 {
-for (a = 0; a < 9; a++)
+for (a = 0; a < 15; a++)
+{
+if (a < 10)
 {
 _putchar(a + '0');
+}
+else
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
 }
 }
 _putchar('\n');
