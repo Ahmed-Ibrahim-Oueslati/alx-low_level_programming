e93131b331eb1eafc433d44446fd8e6d4497ebc4
primitive12 <oueslatiahmed22@gmail.com>
2024-01-27 18:19:07 -0800

    first test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,16 +14,9 @@ int a;
 int b;
 for (b = 0; b < 10; b++)
 {
-for (a = 0; a < 15; a++)
-{
-if (a < 10)
+for (a = 0; a < 9; a++)
 {
 _putchar(a + '0');
-}
-else
-{
-_putchar((a / 10) + '0');
-_putchar((a % 10) + '0');
 }
 }
 _putchar('\n');
