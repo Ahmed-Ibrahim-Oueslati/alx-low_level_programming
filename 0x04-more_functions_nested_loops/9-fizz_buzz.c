a4e7ddbd7e895feb999d860b33e4f6958672f1b8
primitive12 <oueslatiahmed22@gmail.com>
2024-01-29 15:02:26 -0800

    first ets

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i; 
+for (i = 0; i < 101; i++)
+{
+printf("%d ", i);
+}
+putchar('\n');
+return(0);
+}
