82ceb989b66e32a791d9a42d17b48d11e4571026
primitive12 <oueslatiahmed22@gmail.com>
2024-01-29 14:52:21 -0800

    printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - Entry point of the program.
+ *
+ * Description: Prints a square in the terminal
+ *@size: parameter to print
+ * Return: void.
+ */
+void  print_square(int size)
+{
+int p;
+int b;
+if (size > 0)
+{
+for (b = 0; b < size; b++)
+{
+for (p = 0; p < size; p++)
+{
+_putchar('#');
+}
+_putchar(10);
+}
+}
+else
+{
+_putchar(10);
+}
+}
