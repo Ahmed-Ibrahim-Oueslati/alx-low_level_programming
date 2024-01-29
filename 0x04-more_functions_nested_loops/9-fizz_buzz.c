b7ce766d384ab99b4c09c34923a1865b61eabc52
primitive12 <oueslatiahmed22@gmail.com>
2024-01-29 15:16:51 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,22 @@ int main(void)
 int i;
 for (i = 0; i < 101; i++)
 {
+if (((i % 3) != 0) && ((i % 5) != 0)  &&  i != 0)
+{
 printf("%d ", i);
+}
+if (((i % 3) == 0) && ((i % 5) == 0)  &&  i != 0)
+{
+printf("FizzBuzz ");
+}
+if (((i % 3) == 0) && ((i % 5) != 0)  &&  i != 0)
+{
+printf("Fizz ");
+}
+if (((i % 3) != 0) && ((i % 5) == 0)  &&  i != 0)
+{
+printf("Buzz ");
+}
 }
 putchar('\n');
 return (0);
