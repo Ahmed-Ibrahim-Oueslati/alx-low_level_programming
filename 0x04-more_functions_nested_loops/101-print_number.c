0504012c149f07254afd8a711cadad0f525a0d9f
primitive12 <oueslatiahmed22@gmail.com>
2024-02-06 15:34:09 -0800

    printing simply an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,16 +12,17 @@ void print_number(int n)
 {
 int reversed = 0;
 if (n < 0)
-n = n * -1;
 {
+n = n * -1;
+_putchar('-');
+}
+
 if (n < 10)
 {
-_putchar('-');
 _putchar(n + '0');
 }
 if (n > 10)
 {
-_putchar('-');
 while (n > 0)
 {
 int nuh = n % 10;
@@ -33,29 +34,6 @@ while (reversed > 0)
 int digit = reversed % 10;
 _putchar(digit + '0');
 reversed /= 10;
-}
-}
-}
-if (n >= 0)
-{
-if (n < 10) 
-{
-_putchar(n + '0');
-}
-if (n > 10)
-{
-while (n > 0)
-{
-int nuh = n % 10;
-reversed = reversed * 10 + nuh;  
-n /= 10;   
-}
-while (reversed > 0)
-{
-int digit = reversed % 10;
-_putchar(digit + '0');
-reversed /= 10;
-}
 }
 }
 }
