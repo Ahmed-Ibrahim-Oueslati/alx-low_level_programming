f586ec04dc12dddb721e29939905f4b591daeace
primitive12 <oueslatiahmed22@gmail.com>
2024-02-14 09:53:45 +0100

    copying a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,23 +4,23 @@
 /**
  * _strcpy - Entry point of the program.
  *
- * Description: copies the string pointed to by src, including 
+ * Description: copies the string pointed to by src, including
  * the terminating null byte (\0), to the buffer pointed to by dest.
- *@dest:
- *@src:
+ *@dest: string to be copied into
+ *@src:string to be copied
  * Return: a pointer to string .
  */
 char *_strcpy(char *dest, char *src)
-{ 
+{
 int j = 0, i;
-    while (src[j] != '\0')
+while (src[j] != '\0')
 {
 j++;
 }
 j = j - 1;
-for (i=0; i < j; i++)
+for (i = 0; i < j; i++)
 {
-dest[i] = src[i] ;
+dest[i] = src[i];
 }
 dest[j] = '\0';
 return (dest);
