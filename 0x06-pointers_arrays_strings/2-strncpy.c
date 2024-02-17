5332c887ca67065d5746a86162c6da90a4acab67
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-02-17 19:06:49 +0100

    copying a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - Entry point of the program.
+ *
+ * Description: copies a string.
+ *@dest: string 1
+ *@src: string 2
+ *@n: nb of bytes
+ * Return: Always 0 (success).
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int j = 0, i;
+while (src[j] != '\0')
+{
+j++;
+}
+for (i = 0; i <= n; i++)
+{
+dest[i] = src[i];
+}
+dest[j] = '\0';
+return (dest);
+}
