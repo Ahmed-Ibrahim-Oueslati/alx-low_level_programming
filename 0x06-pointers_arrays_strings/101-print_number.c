74ae9e4f5a20633ec31d10a679ad43b6cde0b129
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-04-21 02:16:34 +0100

    trying new approach

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,40 +1,25 @@
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_number - Entry point of the program.
- *
- * Description: prints an integer.
- *
- * Return: Always 0 (success).
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
-int reversed = 0;
-if (n < 0)
-{
-n = n * -1;
-_putchar('-');
-}
-if (n < 10)
-{
-_putchar(n + '0');
-}
-if (n > 10)
-{
-while (n > 0)
-{
-int nuh = n % 10;
-reversed = reversed * 10 + nuh;
-n /= 10;
-}
-while (reversed > 0)
-{
-int digit = reversed % 10;
-_putchar(digit + '0');
-reversed /= 10;
-}
-}
-}
+	unsigned int x;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
