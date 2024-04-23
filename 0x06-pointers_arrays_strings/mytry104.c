247b007c55333f65c97d8c1ca80de6c8b2983d6a
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-04-23 15:17:33 +0100

    printing buffer

---
+++ 0x06-pointers_arrays_strings/mytry104.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description:a function that prints a buffer.
+ *
+ * Return: Always 0 (success).
+ */
+void print_buffer(char *b, int size)
+{
+return (0);
+}
