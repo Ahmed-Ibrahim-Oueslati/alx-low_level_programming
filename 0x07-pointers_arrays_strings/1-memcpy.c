440849b927525efd07e9103d96a850282760e8d3
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-06 16:08:18 +0100

    copy n bytes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * Description: Prints a string to the console.
  *@dest: memory area 2
  *@src: the  memory area 1
- *@n: number of bytes to copy 
+ *@n: number of bytes to copy
  * Return: Always 0 (success).
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
