364c61a2e71c6060f161540dae8d20996c285424
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-06 15:02:51 +0100

    print n ch of buffer

--- 0x07-pointers_arrays_strings/my0-memset.c
+++ 0x07-pointers_arrays_strings/my0-memset.c
@@ -1,5 +1,6 @@
+#include <stdlib.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _memset - function fill the first @n bytes of the memory area pointed
  * to by @s with the constant byte @b
@@ -14,13 +15,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+unsigned int i;
+for (i = 0; i < n; i++)
 
-	while (i < n)
-	{
-	s[i] = b;
-	i++;
-	}
-
-	return (s);
+{
+s[i] = b;
+}
+return (0);
 }
