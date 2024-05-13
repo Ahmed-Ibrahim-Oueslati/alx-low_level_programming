bbaface0a863a04b0816eae070c04ace033a2722
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-13 07:45:31 +0100

    searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strchr - Entry point of the program.
  *
@@ -30,7 +31,28 @@ return (NULL);
 }
 return (p);
 }
+/**
+ * _strncpy - Entry point of the program.
+ *
+ * Description: copies a string.
+ *@dest: string 1
+ *@src: string 2
+ *@n: nb of bytes
+ * Return: Always 0 (success).
+ */
+char *_strncpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
 
+
+return (dest);
+}
 /**
  * _strpbrk- Entry point of the program.
  *
@@ -56,15 +78,26 @@ while (s[j] != '\0')
         printf("Memory allocation failed.\n");
         return NULL;
     }
-for (i = 0; accept[i] != '\0'; i++)
+/* for (i = 0; s[i] != '\0'; i++)
 {
-if ((accept[i] <= 'z' && accept[i] >= 'a') || (accept[i] <= 'Z' && accept[i] >= 'A' ))
+if ((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A' ))
 {
-h[i] = *_strchr(s, accept[i]);
+h[i] = *_strchr(accept, s[i]);
 }
 else 
-h[i] = accept[i];
+h[i] = s[i];
 }
-free(h);
-return (h);
+
+return (h); */
+
+ for (i = 0; s[i] != '\0'; i++) {
+        if (_strchr(accept, s[i]) != NULL) {
+            _strncpy(h, &s[i]); /* Copy substring of s starting from s[i] into h*/
+            return h;
+        }
+    }
+
+    /* If no character from accept is found in s*/
+    free(h); /* Free the allocated memory before returning NULL*/
+    return NULL;
 }
