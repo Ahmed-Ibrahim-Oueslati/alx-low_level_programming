fffac1b027e38f40fe786d926dcb71ffb69fec12
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-15 18:40:26 +0100

    print the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@
 void print_chessboard(char (*a)[8])
 {
 int i, j;
-for (i = 0; i < 2 ; i++)
+for (i = 0; i < 8 ; i++)
 {
 for (j = 0 ; j < 8; j++)
 {
@@ -20,13 +20,4 @@ _putchar(a[i][j]);
 printf("\n");
 }
 printf("\n");
-for (i = 6; i < 8 ; i++)
-{
-for (j = 0 ; j < 8; j++)
-{
-_putchar(a[i][j]);
-}
-printf("\n");
-}
-
 }
