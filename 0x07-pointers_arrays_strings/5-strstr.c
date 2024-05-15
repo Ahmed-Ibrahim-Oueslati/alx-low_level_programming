68d41e3bedcadb6892f4f7c17e1b55275d656531
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-15 17:36:50 +0100

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,6 +22,14 @@ i++;
 
 return (dest);
 }
+/**
+ * _strstr - Entry point of the program.
+ *
+ * Description: locates a substring.
+ *@haystack: string 1
+ *@needle: string 2
+ * Returns a pointer to the beginning of the located substring, or NULL if the substring is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
 int haystack_len = 0;
