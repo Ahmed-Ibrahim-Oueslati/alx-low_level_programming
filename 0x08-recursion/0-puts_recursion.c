99bb3ad726b0743a724eeb9f91a71f4a553a7333
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-20 23:28:42 +0100

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,30 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
+
 /**
- * _puts_recursion- Entry point of the program.
- *
- * Description: prints a string, followed by a new line.
- *@s:string to print
- * Return: Always 0 (success).
- */
+* print_all_letters_starting_from - Entry point of the program.
+* Description:Prints all letters starting from a given position in a string.
+* @s: The string to print from.
+*/
+void print_all_letters_starting_from(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_putchar(*s);
+print_all_letters_starting_from(s + 1);
+}
+
+/**
+* _puts_recursion - Prints a string, followed by a new line.
+* @s: The string to print.
+*/
 void _puts_recursion(char *s)
 {
-int j, i;
-while (s[j] != '\0')
-j++;
-
-for (i = 0; i < j; i++)
-{
-_putchar(s[i]);
-}
+print_all_letters_starting_from(s);
 _putchar('\n');
 }
+
+
