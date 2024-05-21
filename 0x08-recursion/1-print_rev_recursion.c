4bdd70e5d344a417f05a899af5ff0141913cc6fd
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-21 15:31:07 +0100

    recursion and  reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,56 +1,58 @@
-#include <stdlib.h>
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 /**
 * length_string - Calculates the length of a string recursively.
 * @s: The string to calculate length from.
 * Return: The length of the string.
 */
 int length_string(char *s)
+
 {
 if (*s == '\0')
-{
-
 return 0;
- 
-}
 return 1 + length_string(s + 1);
 }
-/**
-* rev_string - Reverses a string recursively.
-* @s: The string to reverse.
-* @h: Buffer to store the reversed string.
-* @length: The length of the string.
-* Return: A pointer to the reversed string.
-*/
-char *rev_string(char *s, char *h, int length)
-{/*do it with a loop , make sure the rest of the code works then try something else : ok */
-if (*s == '\0')
+
+void rev_string_recursive(char *s, int start, int end)
+
 {
-h[length] = '\0';
+char temp;
+if (start >= end)
+return;
 
-return h;
-}
-h[length] = *s;
-return rev_string(s + 1, h, length - 1);
+/*Swap the characters*/
+temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+
+/*Recur for the next pair*/
+rev_string_recursive(s, start + 1, end - 1);
 }
 
 /**
-* print_all_letters_starting_from - Entry point of the program.
-* Description:Prints all letters starting from a given position in a string.
+* rev_string - Reverses a string in place.
+* @s: The string to reverse.
+*/
+void rev_string(char *s)
+
+{
+int len = length_string(s);
+rev_string_recursive(s, 0, len - 1);
+}
+
+/**
+* print_all_letters_starting_from - Prints all letters starting from a given position in a string.
 * @s: The string to print from.
 */
 void print_all_letters_starting_from(char *s)
 
 {
-
 if (*s == '\0')
-
-{
- 
 return;
-}
-_putchar(*s);
+putchar(*s);
 print_all_letters_starting_from(s + 1);
 }
 
@@ -61,36 +63,22 @@ print_all_letters_starting_from(s + 1);
 void _puts_recursion(char *s)
 
 {
-print_all_letters_starting_from(s);
-
+if (*s == '\0')
+return;
+printf("%c", *s);
+_puts_recursion(s + 1);
 }
-/**
-* _print_rev_recursion - Entry point of the program.
-*
-* Description: prints a string in reverse.
-*
-* Return: Always 0 (success).
-*/
 
+/**
+* _print_rev_recursion - Prints a string in reverse.
+* @s: The string to print in reverse.
+*/
 void _print_rev_recursion(char *s)
 
 {
-    char *reversed;
-    printf("lenght is %d\n", length_string(s));
-
-reversed = malloc(length_string(s) + 1); 
-    if (!reversed) {
-        printf("failed");
-        return;
-    }
-reversed[0] = '1';
-    printf("the string is %s \n", reversed);
-    printf("lenght is %d\n", length_string(reversed));
-  reversed= rev_string(s, reversed, length_string(s));
-   printf("lenght2 is %d\n", length_string(reversed));
-  printf("the string is %s \n", s);
-    _puts_recursion(s);
+if (*s == '\0')
+return;
+_print_rev_recursion(s + 1);
+putchar(*s);
 }
-
-
 
