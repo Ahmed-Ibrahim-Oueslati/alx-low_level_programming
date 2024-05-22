8d6a851b41c4e18a1a231e5acc6b620f353b51c3
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-22 09:43:45 +0100

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen_recursion- Entry point of the program.
- *
- * Description:  returns the length of a string
- *@s: string 
- * Return: Always 0 (success).
- */
+* _strlen_recursion- Entry point of the program.
+*
+* Description:  returns the length of a string
+*@s: string
+* Return: Always 0 (success).
+*/
 int _strlen_recursion(char *s)
+
 {
 if (*s == '\0')
 return (0);
