f1551b0709d88e7afe81bec6c617c3abb0eef840
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-22 10:31:13 +0100

    x power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+* _pow_recursion - Entry point of the program.
+*
+* Description: returns the value of x raised to the power of y
+*@x:base
+*@y:exponent
+* Return: The result of x raised to the power
+* of y, or -1 if y is negative..
+*/
+int _pow_recursion(int x, int y)
+
+
+
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+
+return (x * _pow_recursion(x,  y - 1));
+}
