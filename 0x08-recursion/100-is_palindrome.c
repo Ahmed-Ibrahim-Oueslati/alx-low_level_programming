e96f9ce5867b9bd3e646941cc24870981fef7821
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-26 02:33:22 +0100

    is it palindrome ??

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,34 @@
 #include <stdlib.h>
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- * checker - Recursively checks if a string is a palindrome.
- * @s: The string to check.
- * @x: The starting index.
- * @y: The ending index.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
+* checker - Recursively checks if a string is a palindrome.
+* @s: The string to check.
+* @x: The starting index.
+* @y: The ending index.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
 int checker(char *s, int x, int y)
 
 {
-if (x >= y)/* Base case: When indices have met or crossed */
+if (x >= y) /* Base case: When indices have met or crossed */
 return (1);
 
 if (s[x] != s[y]) /* If characters at current indices do not match */
 return (0);
 
-return (checker(s, x - 1, y + 1));
+return (checker(s, x + 1, y - 1)); /* Recur for the next pair of characters */
 }
 
 /**
-* _strlen_recursion- Entry point of the program.
+* _strlen_recursion - Returns the length of a string.
+* @s: The string whose length is to be calculated.
 *
-* Description:  returns the length of a string
-*@s: string
-* Return: Always 0 (success).
+* Return: The length of the string.
 */
 int _strlen_recursion(char *s)
-
 
 {
 if (*s == '\0')
@@ -38,20 +37,16 @@ return (1 + _strlen_recursion(s + 1));
 }
 
 /**
-* is_palindrome - Entry point of the program.
+* is_palindrome - Checks if a string is a palindrome.
+* @s: The string to check.
 *
-* Description: check if a string is a palindrome
-*@s: string to check
-* Return: 1 if a string is a palindrome and 0 if not.
+* Return: 1 if the string is a palindrome, 0 otherwise.
 */
 int is_palindrome(char *s)
 
 {
-
-int x;
-x = _strlen_recursion(s);
-if (x < 2)
+int len = _strlen_recursion(s);
+if (len < 2)
 return (1);
-
-return (checker(s, x - 1, 0));
+return (checker(s, 0, len - 1));
 }
