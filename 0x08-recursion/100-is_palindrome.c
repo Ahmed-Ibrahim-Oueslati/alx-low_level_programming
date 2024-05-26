52f8697ae67c05cf8c6c7eab84c122184a0ed79c
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-26 02:16:02 +0100

    is it palindrome ??

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * checker - Recursively checks if a string is a palindrome.
+ * @s: The string to check.
+ * @x: The starting index.
+ * @y: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int checker(char *s, int y, int x)
+
+{
+if (s[x] != s[y])
+return (0);
+if (x >= y)
+return (1);
+return (checker(s, x + 1, y - 1));
+}
+
+/**
+* _strlen_recursion- Entry point of the program.
+*
+* Description:  returns the length of a string
+*@s: string
+* Return: Always 0 (success).
+*/
+int _strlen_recursion(char *s)
+
+
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+* is_palindrome - Entry point of the program.
+*
+* Description: check if a string is a palindrome
+*@s: string to check
+* Return: 1 if a string is a palindrome and 0 if not.
+*/
+int is_palindrome(char *s)
+
+{
+
+int x;
+x = _strlen_recursion(s);
+if (x < 2)
+return (1);
+
+return (checker(s, x - 1, 0));
+}
