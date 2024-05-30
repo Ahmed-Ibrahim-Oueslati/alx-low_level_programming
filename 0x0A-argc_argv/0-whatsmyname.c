abc5b86303c7648a4aea58ca96cf22bd0a0a2d3a
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-30 12:02:05 +0100

    prg that prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
 *
 * Description: Prints a string to the console.
 *@argc:nb of arguments
-*@argv:the listing of elemnts 
+*@argv:the listing of elemnts
 * Return: Always 0 (success).
 */
 int main(int argc, char **argv)
