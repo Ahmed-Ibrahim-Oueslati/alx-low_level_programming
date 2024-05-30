910092f5f95684fe758ee5ac4495cdd0adbead9a
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-30 14:16:50 +0100

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - Entry point of the program.
- *@argc:nb of arguments
- *@argv:the listing of elemnts
- * Description: Prints a string to the console.
- *
- * Return: Always 0 (success).
- */
+* main - Entry point of the program.
+*@argc:nb of arguments
+*@argv:the listing of elemnts
+* Description: Prints a string to the console.
+*
+* Return: Always 0 (success).
+*/
 int main(int argc, char **argv)
+
 {
 int sum;
 (void)argc;
+if (argc == 1)
 
+{
+printf("Error\n");
+return (0);
+}
 sum = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", sum);
+
 return (0);
 }
