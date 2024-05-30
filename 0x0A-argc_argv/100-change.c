b225e0be71970aede669352f07c6f05b2d142d19
primitive12 <oueslatiahmed22@gmail.com>
2024-05-30 14:31:13 -0700

    min nb of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,36 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
 * main - Entry point of the program.
-*@argc:nb of arguments
-*@argv:the listing of elemnts
-* Description: Prints a string to the console.
+* @argc: Number of arguments
+* @argv: The listing of elements
+* Description: Calculates the minimum number of coins needed to make change.
 *
-* Return: Always 0 (success).
+* Return: Always 0 (success), or 1 if there is an error.
 */
 int main(int argc, char **argv)
-{
 
+{
 int change, x, c25, c10, c5, c2, c1;
 change = c25 = c10 = c5 = c2 = c1 = 0;
-if (argc > 2)
+
+/* Check if the number of arguments is not equal to 2 */
+if (argc != 2)
+
 {
 printf("Error\n");
 return (1);
 }
 
+/* Convert the first argument to an integer */
 x = atoi(argv[1]);
 
-if (x > 0)
+/* Validate the converted integer */
+if (x <= 0)
+
 {
-if ((x / 25) < x )
-{
-c25 = x / 25; 
-/* i'm trying to find the minimum nb of coins of  of 25 10 5 2 and cents could u help ? */
-}
+printf("0\n");
+return (0);
 }
 
+/* Calculate the number of coins needed */
+c25 = x / 25;
+x %= 25;
+c10 = x / 10;
+x %= 10;
+c5 = x / 5;
+x %= 5;
+c2 = x / 2;
+x %= 2;
+c1 = x;
+
+/* Calculate the total number of coins */
 change = c25 + c10 + c5 + c2 + c1;
+
+/* Print the result */
 printf("%d\n", change);
-    return (0);
+return (0);
 }
+
