22e6696d4ac220e5bee614efa83efcc9526b15f1
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-30 12:01:32 +0100

    prg that prints it's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - Entry point of the program.
- *
- * Description: Prints a string to the console.
- *
- * Return: Always 0 (success).
- */
+* main - Entry point of the program.
+*
+* Description: Prints a string to the console.
+*@argc:nb of arguments
+*@argv:the listing of elemnts 
+* Return: Always 0 (success).
+*/
 int main(int argc, char **argv)
+
 {
 printf("%s\n", argv[argc - argc]);
 return (0);
