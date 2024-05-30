fb20075ca5f0e1c42b3316a0586c3ba6a01d0f82
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-30 13:57:20 +0100

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point of the program.
+*@argc:nb of arguments
+*@argv:the listing of elemnts
+* Description: Prints a string to the console.
+*
+* Return: Always 0 (success).
+*/
+int main(int argc, char **argv )
+
+{
+int i;
+while (i < argc)
+
+{
+printf("%s\n", *argv++);
+i++;
+}
+return (0);
+}
