8386a2035545a600bdc715de952e679d70b7aab0
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-30 13:42:41 +0100

    printing the nb of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@argc:nb of arguments
+ *@argv:the listing of elemnts
+ * Return: Always 0 (success).
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
