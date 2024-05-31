bec38c4b80884428d81a7975f4d02ceb9f76224f
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-05-31 15:49:23 +0100

    create an array2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,18 +11,27 @@
 */
 char *create_array(unsigned int size, char c)
 
+
 {
 char *p;
+unsigned int i;
 if (size == 0)
+
 {
 return (NULL);
 }
-p = malloc(size * sizeof(char));
+p = (char *)malloc(size * sizeof(char));
+
 if (p == NULL)
+
 {
-printf("something went wrong");
-return (0);
+return (NULL);
 }
-p[0] = c;
+
+for (i = 0; i < size; i++)
+
+{
+p[i] = c;
+}
 return (p);
 }
