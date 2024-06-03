0b8e2e6799827ea12e2e065a8da2f4dc9c5e5c6a
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-03 01:01:27 +0100

    concat strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+* string_nconcat - Entry point of the program.
+* Description: concatenates two strings.
+*@s1: string 1
+*@s2: string 2
+*@n: nb of bytes
+* Return: a pointer to s1 or NULL
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j, k, l;
+char *p;
+i = 0;
+while (s1[i] != '\0')
+{
+i++;
+}
+while (s2[j] != '\0')
+{
+j++;
+}
+if (n > j)
+{
+n = j;
+}
+k = n + i + 1;
+p = malloc(sizeof(char) * k);
+for (l = 0; l < i; l++)
+{
+p[l] = s1[l];
+}
+for (l = 0; l < n; l++)
+{
+p[l + i] = s2[l];
+}
+p[k] = '\0';
+return (p);
+}
--- 0x0C-more_malloc_free/betty_style_enforcment.sh
+++ 0x0C-more_malloc_free/betty_style_enforcment.sh
@@ -2,3 +2,5 @@
 ./betty_script.sh "$1"
 ./betty_script2.sh "$1"
 ./betty_script3.sh "$1"
+./empty_line.sh "$1"
+./return.sh "$1"
