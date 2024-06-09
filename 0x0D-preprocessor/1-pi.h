78608d493d635c2cab6014d10bb08e7c64d78418
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-09 23:48:35 +0100

    pi macro , first macro

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,2 +1,12 @@
+/**
+ * @file 1-pi.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #pragma once
 #define PI 3.14159265359
