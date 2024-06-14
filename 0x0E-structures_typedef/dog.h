2c34fc995aaa15eda54ce39a06e89e29089e02d1
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-15 00:26:40 +0100

    new type of a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,15 @@
  *@owner: owner of the dog
  * Description : a new struct for dogs.
  */
-typedef struct dog 
+ struct dog 
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif
