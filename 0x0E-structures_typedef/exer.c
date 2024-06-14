6c0c2a9516665ae79913bbce4f27472ad1ad40e7
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-14 16:42:16 +0100

    c book exmaples

--- 0x0E-structures_typedef/exer.c
+++ 0x0E-structures_typedef/exer.c
@@ -8,7 +8,20 @@
  *
  * Return: Always 0 (success).
  */
+typedef struct 
+{
+int age;
+char *name;
+} noswar9a;
+
+typedef int uu, u, o, ar[12];
+typedef char *arr[2][55];
 int main(void)
 {
+arr h;
+h[1][0] = "samira";
+noswar9a ko = { .name = "ahmed "};
+printf("%d\n, %s\n", ko.age, ko.name);
+printf("%s\n", h[1][0]);
 return (0);
 }
