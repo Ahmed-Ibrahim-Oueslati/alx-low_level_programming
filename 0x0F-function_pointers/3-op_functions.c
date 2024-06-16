4108a2a40c514dda115956a715c81be31f1e40ab
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-16 17:08:05 +0100

    update 2

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,91 @@
-#include "3-calc.h"
 #include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
-
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_add - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+
+
+
 /**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_sub - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+
+
+
+
 /**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mul - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+
+
+
 /**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_div - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
+return (a / b);
+}
+
+
 
 /**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mod - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
+return (a % b);
+}
+
+
