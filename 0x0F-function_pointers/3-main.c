128577f521faad37d37ba02e977d83a3d82a389a
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-16 16:12:15 +0100

    a prg that  performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@ac: nb of arguments
+ *@av: array of pts to the args
+ * Return: Always 0 (success).
+ */
+int main(int ac, char **av)
+{
+int num1, num2, resl;
+char *operator;
+int (*res)(int, int);
+if (ac != 4)
+{
+printf("Error1\n");
+exit(98);
+}
+num1 = atoi(av[1]);
+num2 = atoi(av[3]);
+operator = av[2];
+if (*operator != '+' && *operator != '-' &&
+*operator != '*' && *operator != '/' && *operator != '%')
+{
+printf("Error2\n");
+exit(99);
+}
+if ((*operator == '/' || *operator == '%') && (num1 == 0 || num2 == 0))
+{
+printf("Error3\n");
+exit(100);
+}
+
+
+res = get_op_func(operator);
+if (res == NULL)
+{
+printf("Error4\n");
+return (99);
+}
+resl = op_add(num1, num2);
+printf("%d\n", resl);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * op_add - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
+int op_add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
+
+
+
+
+/**
+ * op_sub - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
+int op_sub(int a, int b)
+{
+int dif;
+dif = a - b;
+return (dif);
+}
+
+
+
+
+
+/**
+ * op_mul - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
+int op_mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
+
+
+
+
+/**
+ * op_div - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
+int op_div(int a, int b)
+{
+int div;
+div = a / b;
+return (div);
+}
+
+
+
+/**
+ * op_mod - Entry point of the program.
+ *
+ * Description: Prints a string to the console.
+ *@a: first int
+ *@b: second int
+ * Return: Always 0 (success).
+ */
+int op_mod(int a, int b)
+{
+int mod;
+mod = a % b;
+return (mod);
+}
+
+
