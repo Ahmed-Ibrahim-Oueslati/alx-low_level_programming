befa5baa9d6c5874ee9adc864d87da3ade7b0220
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-18 20:19:58 +0100

    update

--- 0x07-pointers_arrays_strings/sebn_work/pwd.c
+++ 0x07-pointers_arrays_strings/sebn_work/pwd.c
@@ -15,13 +15,15 @@ int i ;
 char *pwd1;
 char *list1;
 
-char *arr[21][1]= {"Adil.Semrich", "Aziz.Guagua", "Aziz.Sahili", "Badr.Barakat", "Fatima.Hanfar", "jilali.hani", "Mohamed.ElIdrissi", "Rachid.Bchiri", "Saad.Begdouri", "Simohammed.Kjairi", "Yassin.Boulbene", "Yassin.Tamsamani", "Aziz.Souidi", "Aya.Elfares", "Charaf-eddine.Zellaj", "Hicham.Elaffaki", "Mohamed.Ibnelfaqyh", "Mohammed.Rhoddane", "Oum-elmouminine.Elmo", "Junichi.Kato", "Zhor.Chahir"};
+char *arr[18][1]= {"oihhh.llmp"};
 
-for (i = 0; i < 21; i++)
+for (i = 0; i < 18; i++)
 {
 list1 = *arr[i];
  pwd1 = _pmaking(list1);
  printf("%s\n " , pwd1);
 }
+
+
 return (0);
 }
--- 0x07-pointers_arrays_strings/sebn_work/vpn_pwd.c
+++ 0x07-pointers_arrays_strings/sebn_work/vpn_pwd.c
@@ -3,80 +3,116 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main - Entry point of the program.
- *
- * Description: Prints a string to the console.
- *
- * Return: Always 0 (success).
- */
+* main - Entry point of the program.
+*
+* Description: Prints a string to the console.
+*
+* Return: Always 0 (success).
+*/
 char *_pmaking(char *list)
+
 {
 char *pwd;
-int i; 
+int i;
 
 
 
-  /* Allocate memory for pwd */
-    pwd = (char *)malloc(strlen(list) + 1); // +1 for the null terminator '\0'
-    if (pwd == NULL) {
-        printf("Memory allocation failed.\n");
-        return NULL;
-    }
+/* Allocate memory for pwd */
+pwd = (char *)malloc(strlen(list) + 1); // +1 for the null terminator '\0'
+if (pwd == NULL)
+{
+printf("Memory allocation failed.\n");
+return NULL;
+}
 
 if ( list[0] >= 'a'   &&  list[0] <= 'z')
 
+
 {
 
-    pwd[0] = list[0] - 32 ;
+pwd[0] = list[0] - 32 ;
 }
- 
- else 
-  pwd[0] = list[0];
+
+
+else
+pwd[0] = list[0];
+
+
+if (pwd[0] == 'I' || pwd[0] == 'O' )
+
+{
+pwd[0] = 'T';
+}
 
 if ( list[1] >= 'a'  &&  list[1] <= 'z')
+
 
 {
 pwd[1] = list[1] - 32 ;
 }
+else
+pwd[1] = list[1];
 
- else 
-  pwd[1] = list[1];
 
 
- pwd[2] = '.';
+if (pwd[1] == 'I' || pwd[1] == 'O' )
+
+{
+pwd[1] = 'T';
+}
+
+
+pwd[2] = '.';
 
 
 for ( i = 0; list[i] != '\0'; i++ )
+
 {
 if (list[i] == '.')
 
+
 {
-  if ( list[i+1] >= 'A'   &&  list[i+1] <= 'Z')
+if ( list[i+1] >= 'A'   &&  list[i+1] <= 'Z')
+
 
 {
 
-    pwd[3] = list[i+1] + 32 ;
+pwd[3] = list[i+1] + 32 ;
 }
-else 
-    pwd[3] = list[i+1];
+else
+pwd[3] = list[i+1];
+
+if (pwd[3] == 'l')
+
+{
+pwd[3] = 'u';
+}
 
 
- if ( list[i+2] >= 'A'   &&  list[i+2] <= 'Z')
+if ( list[i+2] >= 'A'   &&  list[i+2] <= 'Z')
+
 
 {
 
-    pwd[4] = list[i+2] + 32 ;
+pwd[4] = list[i+2] + 32;
 }
-else 
-  pwd[4] = list[i+2];
+else
+pwd[4] = list[i+2];
 
-    
+if (pwd[4] == 'l')
+
+{
+pwd[4] = 'u';
+}
+
+
 }
 }
 
 pwd[i] = '\0';
- /* printf("the user name is : %s \n the pwd is %s \n " , list, pwd); */ 
+/* printf("the user name is : %s \n the pwd is %s \n " , list, pwd); */
 
 
 return (pwd);
+
 }
