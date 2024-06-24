c4e39630b3fb7868300923f7c055131698777147
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-24 01:03:34 +0100

    printing nbs with variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - prints numbers, followed by a new line
+* @n: The number of parameters.
+* @separator: the separator between the nbs
+* Return: Always 0 (success).
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+
+{
+va_list ap;
+unsigned int i;
+
+
+
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+
+
+{
+if (separator != NULL)
+{
+	printf("%d%s", va_arg(ap, int), separator);
+}
+else
+
+{
+	printf("%d", va_arg(ap, int));
+}
+}
+
+va_end(ap);
+
+putchar('\n');
+
+}
