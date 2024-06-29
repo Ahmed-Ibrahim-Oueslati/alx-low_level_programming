93d485ba62479956cd204e95bc4a64546a7c7008
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-29 02:58:15 +0100

    prints anything , char , int , float char *

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_all - Prints various types of arguments based on a format string.
+*
+* @format: A string representing the types of arguments passed.
+*          'c' for char, 'i' for int, 'f' for float, 's' for string.
+* @...: A variable number of arguments.
+* Return: void.
+*/
+void print_all(const char * const format, ...)
+
+{
+va_list ap;
+int i = 0;
+char *str;
+char c;
+
+va_start(ap, format);
+
+while (format && format[i])
+
+{
+switch (format[i])
+
+{
+case 'c':
+c = va_arg(ap, int);
+printf("%c", c);
+break;
+case 'i':
+printf("%d", va_arg(ap, int));
+break;
+case 'f':
+printf("%f", va_arg(ap, double));
+break;
+case 's':
+str = va_arg(ap, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+break;
+default:
+break;
+}
+if (format[i + 1])
+printf(", ");
+i++;
+}
+
+va_end(ap);
+putchar('\n');
+}
+
