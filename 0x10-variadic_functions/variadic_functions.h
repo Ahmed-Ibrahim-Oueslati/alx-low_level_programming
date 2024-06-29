559310f1a334d7fcef7e8b28aa50fee4200e9796
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-06-29 03:25:02 +0100

    prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,13 @@
 #ifndef VAR
 #define VAR
-#include <stddef.h> 
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
 
 #endif
