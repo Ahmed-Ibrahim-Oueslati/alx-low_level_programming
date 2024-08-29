e0986604d15c64ed7f3b48e4fd0bb9bdeaaa9bf0
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-08-29 23:54:48 +0100

    update3

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,27 +1,27 @@
-#include "lists.h"
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * Return: number of nodes
- *@h: pointer to first node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
-size_t print_list(const list_t *h)
+typedef struct list_s
 {
-	size_t i = 0;
-	const list_t *traverse = h;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-	while (traverse)
-	{
-		if (traverse->str != NULL)
-		{
-			printf("[%u] ", traverse->len);
-			printf("%s\n", traverse->str);
-		}
-		else
-			printf("[0] (nil)\n");
-		traverse = traverse->next;
-		i++;
-	}
-	return (i);
-}
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
